15524aa3538fefda0d777ba11078a6b7364021c6
Dakshitha Hasindra <hasindradh@gmail.com>
2024-01-24 02:21:57 +0530

    completed fetch all tasks and fetch single task from server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 
 
-const express =  require('express');
+const express = require('express');
 const mongoose = require('mongoose');
 const Product = require('./models/productModel');
 const app = express();
@@ -10,29 +10,51 @@ app.use(express.json());
 
 //routes 
 
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
     res.send("<h1>Hello node API<h1/>");
 })
 
-app.post('/task',async(req, res)=>{
+app.post('/tasks', async (req, res) => {
     try {
         const product = await Product.create(req.body);
         res.status(200).json(product);
     } catch (error) {
         console.log(error.message);
-        res.status(500).json({message: error.message});
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.get('/tasks', async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message });
     }
 });
 
 
-mongoose.set("strictQuery",false);
+app.get('/tasks/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+
+mongoose.set("strictQuery", false);
 mongoose.connect('mongodb://localhost:27017/todo-app-nodejs')
-.then(()=>{
-    console.log("Connected to database");
-    app.listen(3000, ()=>{
-        console.log("Node js server is runnig at port 3000");
-    });
-})
-.catch((error)=>{
-    console.log("Error", error);
-})
+    .then(() => {
+        console.log("Connected to database");
+        app.listen(3000, () => {
+            console.log("Node js server is runnig at port 3000");
+        });
+    })
+    .catch((error) => {
+        console.log("Error", error);
+    })
